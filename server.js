ec282c0e12cea42d03981c2e58103b311356885c
AQUATECHIE <amolequwam96@gmail.com>
2024-08-08 13:25:13 +0100

    g

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ connectDB().then()
 
 const app = express()
 
-app.use(morgan("dev"))
 app.use(cors())
+app.use(morgan("dev"))
 app.use(express.json({limit: "50mb"}))
 app.use(express.urlencoded({extended: "true", limit: "50mb"}))
 app.use(fileUpload());
