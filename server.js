c0d13d292720baa54dc7403f89217f64a73e0b7a
AQUATECHIE <amolequwam96@gmail.com>
2024-09-21 22:47:45 +0100

    AA

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ connectDB().then()
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:3000',  // React frontend URL
+    origin: '*',  // React frontend URL
     credentials: true  // Enable if you're using cookies/auth tokens
 }));
 app.use(morgan("dev"))
