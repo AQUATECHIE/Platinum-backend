33ec5bd35a3e28399fbedb811f5c39fa3b079aef
AQUATECHIE <amolequwam96@gmail.com>
2024-11-04 12:33:02 +0100

    g

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,29 +1,29 @@
-import express from 'express';
-import {login , register, verifyEmail} from "../controllers/authController.js"
-import { validateRegister, validateLogin, handleValidationErrors } from '../middlewares/Validation.js';
-import { UserProfile, updateUserProfile, deleteUser } from '../controllers/authController.js';
-import { protect } from '../middlewares/authMiddleware.js';
+// import express from 'express';
+// import {login , register, verifyEmail} from "../controllers/authController.js"
+// import { validateRegister, validateLogin, handleValidationErrors } from '../middlewares/Validation.js';
+// import { UserProfile, updateUserProfile, deleteUser } from '../controllers/authController.js';
+// import { protect } from '../middlewares/authMiddleware.js';
 
-// import User from '../models/users.js';
+// // import User from '../models/users.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-// User Registration Route with Validation
-router.post('/register', validateRegister, handleValidationErrors, register);
+// // User Registration Route with Validation
+// router.post('/register', validateRegister, handleValidationErrors, register);
 
-// email verification
-router.post('/verify/:token', verifyEmail)
-
-
-// User Login Route with Validation
-router.post('/login', validateLogin, handleValidationErrors, login);
-
-router.route('/profile')
-    .get(protect, UserProfile) // Get single user (READ)
-    .put(protect, updateUserProfile) // Update user (UPDATE)
-    .delete(protect, deleteUser); // Delete user (DELETE)
+// // email verification
+// router.post('/verify/:token', verifyEmail)
 
 
+// // User Login Route with Validation
+// router.post('/login', validateLogin, handleValidationErrors, login);
+
+// router.route('/profile')
+//     .get(protect, UserProfile) // Get single user (READ)
+//     .put(protect, updateUserProfile) // Update user (UPDATE)
+//     .delete(protect, deleteUser); // Delete user (DELETE)
 
 
-export default router;
+
+
+// export default router;
