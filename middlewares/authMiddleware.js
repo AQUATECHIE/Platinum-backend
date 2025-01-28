3d3b859ee3e14812da28c37b7129f0fd0b9e3418
AQUATECHIE <amolequwam96@gmail.com>
2025-01-28 13:50:12 +0100

    chart

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,11 +2,7 @@ import jwt from 'jsonwebtoken';
 // import User from '../models/users.js';
 
 export const protect = (req, res, next) => {
-  let token;
-
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    token = req.headers.authorization.split(' ')[1];
-  }
+  const token = req.header('x-auth-token');
 
   if (!token) {
     return res.status(401).json({ error: 'Not authorized' });
@@ -14,7 +10,7 @@ export const protect = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Add user data to request
+    req.user = decoded.user; // Add user data to request
     next();
   } catch (error) {
     res.status(401).json({ error: 'Invalid token' });
